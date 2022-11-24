00c57f01e87ddc388acf3924f225922187b3b388
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-11-24 16:52:16 +0100

    Create model Book

---
+++ backend/src/main/java/de/neuefische/backend/model/Book.java
@@ -0,0 +1,17 @@
+package de.neuefische.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private String isbn;
+
+}
