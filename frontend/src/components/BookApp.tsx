7fa8148a0a0038fc1c601a57acd2b8180e2a8a24
marzad <maschatz@gmx.net>
2022-11-25 14:23:11 +0100

    Add & fix getBookByID-functionality

--- frontend/src/components/BookApp.tsx
+++ frontend/src/components/BookApp.tsx
@@ -5,14 +5,32 @@ import axios from "axios";
 
 export default function BookApp() {
 const [bookList,setBookList]=useState<BookData[]>([])
+
     useEffect(() => {
         getAllBooks()
     },[])
+
  function getAllBooks() {
     axios.get("books").then(response=>response.data).then(data=>setBookList(data))
 
  }
+
+    const filteredBook = (id: string): BookData[] => {
+        return bookList.filter(book => {
+            return book.id === id
+        })
+    }
+
+ function getBookByID(id: string){
+    if(id){
+        setBookList(filteredBook(id))
+    }
+ }
+
     return (
-        <BookList bookList={bookList}/>
+        <>
+            <BookList bookList={bookList} getBookByIDInBookList={getBookByID}/>
+        </>
+
     )
 }
