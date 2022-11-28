64c18bb4cc73c29afc3a43510150e3d6eadea127
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-11-28 19:19:40 +0100

    Add primitive style of Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,28 @@
+import {Box, Container } from "@mui/material";
 import React from "react";
+import './Home.css'
 
 
 export default function Home() {
     return (
 
 
-            <section>
-                <div>
-                    Wir wissen, dass du Book-Paradise-App bereits kennst, aber es lohnt sich, es zu wiederholen, denn die App macht die Nutzung noch einfacher! Du kannst ein Buch zu Ihrer Favoritenliste speichern, in vorgestellten Büchern und Genres stöbern, sehen, was Ihre Freunde lesen!
+            <section className={"home-text"}>
+                <Container maxWidth="sm">
+                   Wir wissen, dass du Book-Paradise-App bereits kennst, aber es lohnt sich,
+                    es zu wiederholen, denn die App macht die Nutzung noch einfacher!
+                    Du kannst ein Buch zu Ihrer Favoritenliste speichern, in vorgestellten
+                    Büchern und Genres stöbern, sehen, was Ihre Freunde lesen!
+                    Außerdem kannst du jedes Buch im gesamten Book-Paradise-Katalog suchen,
+                    bewerten und rezensieren. Es ist sogar noch einfacher, den Überblick
+                    über all die Bücher zu behalten, die du lesen willst und gelesen hast.
+                    Wir finden es auch toll, dass man sich über die Book-Paradise-App
+                    mit Hilfe von Nachrichten, Rezensionen, Gruppen und Empfehlungen über
+                    Bücher austauschen kann!
+                </Container>
 
-                    Außerdem kannst du jedes Buch im gesamten Book-Paradise-Katalog suchen, bewerten und rezensieren. Es ist sogar noch einfacher, den Überblick über all die Bücher zu behalten, die du lesen willst und gelesen hast. Wir finden es auch toll, dass man sich über die Book-Paradise-App mit Hilfe von Nachrichten, Rezensionen, Gruppen und Empfehlungen über Bücher austauschen kann!
-                </div>
+
+
             </section>
     )
 
