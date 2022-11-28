21a8eaa1bb0b24e377aababe276cd867ece07997
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-11-28 19:37:47 +0100

    Add changes of style of Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import {Box, Container } from "@mui/material";
+import {Box, Container, Typography } from "@mui/material";
 import React from "react";
 import './Home.css'
 
@@ -8,18 +8,24 @@ export default function Home() {
 
 
             <section className={"home-text"}>
-                <Container maxWidth="sm">
-                   Wir wissen, dass du Book-Paradise-App bereits kennst, aber es lohnt sich,
-                    es zu wiederholen, denn die App macht die Nutzung noch einfacher!
-                    Du kannst ein Buch zu Ihrer Favoritenliste speichern, in vorgestellten
-                    Büchern und Genres stöbern, sehen, was Ihre Freunde lesen!
-                    Außerdem kannst du jedes Buch im gesamten Book-Paradise-Katalog suchen,
-                    bewerten und rezensieren. Es ist sogar noch einfacher, den Überblick
-                    über all die Bücher zu behalten, die du lesen willst und gelesen hast.
-                    Wir finden es auch toll, dass man sich über die Book-Paradise-App
-                    mit Hilfe von Nachrichten, Rezensionen, Gruppen und Empfehlungen über
-                    Bücher austauschen kann!
-                </Container>
+                <main>
+                    <div>
+                        <Container maxWidth="sm">
+                            <Typography variant = "h2" align={"center"} color={"textPrimary"} gutterBottom >Home</Typography>
+                            <Typography variant = "h5" align={"center"} color={"textPrimary"} gutterBottom paragraph>Wir wissen, dass du Book-Paradise-App bereits kennst, aber es lohnt sich,
+                                es zu wiederholen, denn die App macht die Nutzung noch einfacher!
+                                Du kannst ein Buch zu Ihrer Favoritenliste speichern, in vorgestellten
+                                Büchern und Genres stöbern, sehen, was Ihre Freunde lesen!
+                                Außerdem kannst du jedes Buch im gesamten Book-Paradise-Katalog suchen,
+                                bewerten und rezensieren. <br /> Es ist sogar noch einfacher, den Überblick
+                                über all die Bücher zu behalten, die du lesen willst und gelesen hast.
+                                Wir finden es auch toll, dass man sich über die Book-Paradise-App
+                                mit Hilfe von Nachrichten, Rezensionen, Gruppen und Empfehlungen über
+                                Bücher austauschen kann!</Typography>
+                        </Container>
+                    </div>
+                </main>
+
 
 
 
