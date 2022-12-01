0c83c858b64378b2e826992b466ea1b974085d56
marzad <maschatz@gmx.net>
2022-12-01 12:10:38 +0100

    Update BookDTO

--- backend/src/main/java/de/neuefische/backend/model/BookDTO.java
+++ backend/src/main/java/de/neuefische/backend/model/BookDTO.java
@@ -1,11 +1,10 @@
 package de.neuefische.backend.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+
 @NoArgsConstructor
 public class BookDTO {
     private String isbn;
