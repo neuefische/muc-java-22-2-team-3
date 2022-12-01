b209521a6eb3a6017229121ac3f8af57862e95b0
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-12-01 17:15:50 +0100

    Fix bug with mongodb

--- backend/src/main/java/de/neuefische/backend/model/Book.java
+++ backend/src/main/java/de/neuefische/backend/model/Book.java
@@ -1,27 +1,37 @@
 package de.neuefische.backend.model;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+
 @Document("books")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class Book {
-@Id
+    @Id
     private String id;
+
     private String isbn;
+
     private String title;
+
     private String subtitle;
+
     private String author;
+
     private String publisher;
+
     private int pages;
+
     private String description;
+
     private String website;
 
-    public Book(String id, String title, String author, String isbn){
+    public Book(String id, String title, String author, String isbn) {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
--- backend/src/main/java/de/neuefische/backend/model/BookDTO.java
+++ backend/src/main/java/de/neuefische/backend/model/BookDTO.java
@@ -1,6 +1,8 @@
 package de.neuefische.backend.model;
 
 
+
+
 public record BookDTO (String isbn,
         String title,
         String author){
