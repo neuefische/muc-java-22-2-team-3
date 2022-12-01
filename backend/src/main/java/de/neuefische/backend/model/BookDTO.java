376aa3c7c536ec9edb955538117645a4f6b49427
marzad <maschatz@gmx.net>
2022-12-01 12:18:23 +0100

    Update BookDTO

---
+++ backend/src/main/java/de/neuefische/backend/model/BookDTO.java
@@ -0,0 +1,10 @@
+package de.neuefische.backend.model;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class BookDTO {
+    private String isbn;
+    private String title;
+    private String author;
+}
