204910f14fcdb0410e1e35cfb868ba038f065046
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-12-02 11:16:19 +0100

    Add getFavoriteBookList bei FavoriteBooksService

--- backend/src/main/java/de/neuefische/backend/service/FavoriteBooksService.java
+++ backend/src/main/java/de/neuefische/backend/service/FavoriteBooksService.java
@@ -1,4 +1,21 @@
 package de.neuefische.backend.service;
-
+import de.neuefische.backend.model.FavoriteBooks;
+import de.neuefische.backend.repository.FavoriteBooksRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
 public class FavoriteBooksService {
+
+    private final FavoriteBooksRepository favouriteBooksRepository;
+
+    private final IDGenerator idGenerator;
+
+    public FavoriteBooksService(FavoriteBooksRepository favouriteBooksRepository, IDGenerator idGenerator) {
+        this.favouriteBooksRepository = favouriteBooksRepository;
+        this.idGenerator = idGenerator;
+    }
+
+    public List<FavoriteBooks> getFavoriteBookList() {
+        return favouriteBooksRepository.findAll();
+    }
 }
