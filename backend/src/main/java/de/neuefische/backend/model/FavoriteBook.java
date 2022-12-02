8a693b2463e6f1a5e8f2dc8dc60705e2c580d16f
marzad <maschatz@gmx.net>
2022-12-02 14:12:57 +0100

    Fix constructor

--- backend/src/main/java/de/neuefische/backend/model/FavoriteBook.java
+++ backend/src/main/java/de/neuefische/backend/model/FavoriteBook.java
@@ -6,26 +6,31 @@ import lombok.Data;
 public class FavoriteBook extends Book{
     Status status;
 
+    public FavoriteBook(){
+    }
+
+    public FavoriteBook(Book book){
+
+        this.setId(book.getId());
+        this.setAuthor(book.getAuthor());
+        this.setIsbn(book.getIsbn());
+        this.setDescription(book.getDescription());
+        this.status = Status.TOREAD;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof FavoriteBook that)) return false;
         if (!super.equals(o)) return false;
 
-        FavoriteBook that = (FavoriteBook) o;
-
-        return status == that.status;
+        return getStatus() == that.getStatus();
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
-        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (getStatus() != null ? getStatus().hashCode() : 0);
         return result;
-    }
-
-    public FavoriteBook(){
-        super();
-        this.status = Status.TOREAD;
     }
 }
