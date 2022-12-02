1c9b7dbef90c61d469d42d15ec7e568a2d58f743
marzad <maschatz@gmx.net>
2022-12-02 12:53:00 +0100

    Fix constructor in FavoriteBook, add equals/hash-methods

--- backend/src/main/java/de/neuefische/backend/model/FavoriteBook.java
+++ backend/src/main/java/de/neuefische/backend/model/FavoriteBook.java
@@ -6,8 +6,26 @@ import lombok.Data;
 public class FavoriteBook extends Book{
     Status status;
 
-    public FavoriteBook(Book book){
-        super(id, title, author, isbn)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        FavoriteBook that = (FavoriteBook) o;
+
+        return status == that.status;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        return result;
+    }
+
+    public FavoriteBook(){
+        super();
         this.status = Status.TOREAD;
     }
 }
