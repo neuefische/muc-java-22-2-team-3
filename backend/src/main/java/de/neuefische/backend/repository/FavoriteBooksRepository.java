929c72e68c050f8320a5d19def605a4eaa49b00b
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-12-02 11:16:19 +0100

    Add getFavoriteBookList bei FavoriteBooksService

--- backend/src/main/java/de/neuefische/backend/repository/FavoriteBooksRepository.java
+++ backend/src/main/java/de/neuefische/backend/repository/FavoriteBooksRepository.java
@@ -1,4 +1,8 @@
 package de.neuefische.backend.repository;
+import de.neuefische.backend.model.FavoriteBooks;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface FavoriteBooksRepository {
+@Repository
+public interface FavoriteBooksRepository extends MongoRepository<FavoriteBooks, String> {
 }
