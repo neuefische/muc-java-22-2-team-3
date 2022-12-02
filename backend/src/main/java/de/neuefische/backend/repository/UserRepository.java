7abbc76bcda05a178afe06b24c3ec4d9dd624554
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-12-02 10:45:35 +0100

    Add getalluser bei userservice

--- backend/src/main/java/de/neuefische/backend/repository/UserRepository.java
+++ backend/src/main/java/de/neuefische/backend/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package de.neuefische.backend.repository;
+import de.neuefische.backend.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
 }
--- backend/src/main/java/de/neuefische/backend/service/UserService.java
+++ backend/src/main/java/de/neuefische/backend/service/UserService.java
@@ -1,4 +1,21 @@
 package de.neuefische.backend.service;
-
+import de.neuefische.backend.model.User;
+import de.neuefische.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final IDGenerator idGenerator;
+
+    public UserService(UserRepository userRepository, IDGenerator idGenerator) {
+        this.userRepository = userRepository;
+        this.idGenerator = idGenerator;
+    }
+
+
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
 }
