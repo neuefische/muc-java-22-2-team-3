d011a4c5147c772a1b37706dae689f03c17f9e5d
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-12-06 17:36:34 +0100

    Add Integrationstest getAllUser

--- backend/src/main/java/de/neuefische/backend/repository/FavoriteBooksRepository.java
+++
@@ -1,8 +0,0 @@
-package de.neuefische.backend.repository;
-import de.neuefische.backend.model.FavoriteBook;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FavoriteBooksRepository extends MongoRepository<FavoriteBook, String> {
-}
--- backend/src/test/java/de/neuefische/backend/controller/UserControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/UserControllerTest.java
@@ -4,25 +4,34 @@ import de.neuefische.backend.model.BookUser;
 import de.neuefische.backend.model.FavoriteBook;
 import de.neuefische.backend.model.BookUser;
 import de.neuefische.backend.repository.UserRepository;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class UserControllerTest {
+
 
     @Autowired
     MockMvc mockMvc;
@@ -31,42 +40,51 @@ class UserControllerTest {
     UserRepository userRepository;
 
 
-
+    @WithMockUser("spring")
     @Test
-    void test_getAllUser() throws Exception {
-
-        Set<String> newSet = new HashSet<>();
-        newSet.add("123");
-        BookUser newUser = new BookUser("1","AntonM", "password", "Anton" , "Mustermann",newSet);
-
-        userRepository.save(newUser);
-
-        ResultActions result = mockMvc.perform(get("/books/users"))
-                .andDo(MockMvcResultHandlers.print())
+    void getAllUser() throws Exception {
+        BookUser user1 = new BookUser( "123", "username", "password",  "firstname",
+                "lastname", new HashSet());
+        userRepository.save(user1);
+        mockMvc.perform(get("/users"))
                 .andExpect(status().isOk())
-
                 .andExpect(content().json("""
-                        [{"id":"1",
-                        "username":"AntonM",
-                        "password":"password",
-                        "firstname":"Anton",
-                        "lastname":"Mustermann",
-                        "favoriteBookSet":["123"]
-                        }]
+                        [{"id": "123",
+                        "username": "username",
+                        "password": "password",
+                        "firstname": "firstname",
+                        "lastname": "lastname",
+                        "favoriteBookSet":[]}]
+                        
                         """));
-    }
 
+    }
+    @WithMockUser("spring")
     @Test
-    void test_getFavoriteBooks() {
+    void addUser() {
     }
-
+    @WithMockUser("spring")
     @Test
-    void test_addBookToFavorites() {
+    void getFavoriteBooks() {
     }
-
+    @WithMockUser("spring")
     @Test
-    void test_deleteBookFromFavorites() {
+    void addBookToFavorites() {
     }
-
-
+    @WithMockUser("spring")
+    @Test
+    void deleteBookFromFavorites() {
+    }
+    @WithMockUser("spring")
+    @Test
+    void deleteUser() {
+    }
+    @WithMockUser("spring")
+    @Test
+    void login() {
+    }
+    @WithMockUser("spring")
+    @Test
+    void logout() {
+    }
 }
